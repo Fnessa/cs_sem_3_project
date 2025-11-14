375ffb070bea4e664829fcac8adbc6ed213b40fe
Fnessa <contact@fnessa.net>
2025-11-14 13:01:33 +0100

    water spreading

--- Grid.java
+++ Grid.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
+
 public class Grid {
+	final static int treeAmount = 43;
+	final static int fireAmount = 12;
+	final static int rockAmount = 10;
+	final static int waterAmount = 5;
+	//x = column, width
+	//y = row, height
+	final static int width = 10;
+	final static int height = 8;
+
 	public static void main(String[] args) {
-		final int treeAmount = 43;
-		final int fireAmount = 12;
-		final int rockAmount = 10;
-		final int waterAmount = 5;
-		//x = rows, width
-		//y = columns, height
-		int width = 10;
-		int height = 8;
+
 
 		//creates grid
 		Cell[][] grid = new Cell[height][width];
@@ -21,7 +24,7 @@ public class Grid {
 		System.out.printf("Trees: %d, Rocks: %d, Water: %d, Burning trees: %d \n", countObjects(grid)[0], countObjects(grid)[1], countObjects(grid)[2], countObjects(grid)[3]);
 
 		turn(grid);
-		
+
 		outputGrid(grid);
 	}
 	public static void outputGrid(Cell[][] grid){
@@ -125,12 +128,41 @@ public class Grid {
 	public static void turn(Cell[][] grid) {
 		Scanner in = new Scanner(System.in);
 		//prompts user for input and gets the selected x and y coordinates
-		System.out.printf("Row to drop water (1 - %d): ", grid[0].length);
+		System.out.printf("Column to drop water (1 - %d): ", grid[0].length);
 		int x_selected = in.nextInt() - 1;
-		System.out.printf("Column to drop water (1 - %d): ", grid.length);
+		System.out.printf("Row to drop water (1 - %d): ", grid.length);
 		int y_selected = in.nextInt() - 1;
 
+		wave(grid, y_selected + 1, x_selected, 0, 1);
+		wave(grid, y_selected, x_selected + 1, 1, 0);
+		wave(grid, y_selected, x_selected -1, -1, 0);
+		wave(grid, y_selected -1, x_selected, 0, -1);
 		grid[y_selected][x_selected].setType("water");
 
+	}
+	public static boolean wave(Cell[][] grid, int y_selected, int x_selected, int x_direction, int y_direction){
+		int next_x = x_selected + x_direction;
+		int next_y = y_selected + y_direction;
+
+		//if the current cell is rock or water, return false
+		if (grid[y_selected][x_selected].getType() == "rock" || grid[y_selected][x_selected].getType() == "water") {
+			System.out.println("a");
+			return false;
+		}
+		//if the current cell is fire
+		else if (grid[y_selected][x_selected].getType() == "fire"){
+			System.out.println("b");
+			grid[y_selected][x_selected].setType("water");
+			//if the next cell is in grid return recursive wave, else return true
+			if (next_x < width && next_x >= 0 && next_y < height && next_y >= 0) {
+				System.out.println("c");
+				return wave(grid, y_selected + y_direction, x_selected + x_direction, x_direction, y_direction);
+			} else {return true;}
+		}
+		else {
+			if (next_x < width && next_x >= 0 && next_y < height && next_y >= 0) {
+				return wave(grid, y_selected + y_direction, x_selected + x_direction, x_direction, y_direction);
+			} else {return true;}
+		}
 	}
 }
