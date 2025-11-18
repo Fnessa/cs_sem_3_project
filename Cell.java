1a73e56538925bae84effffa5cb7c27b92b9e6ef
Fnessa <contact@fnessa.net>
2025-11-18 21:01:05 +0100

    comments in cell

--- Cell.java
+++ Cell.java
@@ -2,29 +2,36 @@ import java.util.Random;
 
 public class Cell {
 
-	private static String[] types = {"tree", "fire", "rock", "water",};
+	private static String[] types = {"tree", "fire", "rock", "water",}; //types that are generated in the beginning
 	private String type;
-	public String representation;
+	public String representation; //character that represents the cell
 	public boolean flammable = false;
 
+	//Constructor
 	public Cell(String type){
 		this.setType(type);
 	}
 
+	//Setter for the type. Also sets how it is represented by calling setRepresentation
 	public void setType(String type){
 		this.type = type;
 		this.setRepresentation();
 	}
+
 	public String getType(){
 		return this.type;
 	}
 	public String getRepresentation(){
 		return this.representation;
 	}
+	
+	//returns a random type from the types array
 	public static String randomType(){
 		Random r = new Random();
 		return types[r.nextInt(types.length)];
 	}
+
+	//sets representation according to the type of the instance
 	public void setRepresentation(){
 		switch (this.type) {
 			case "tree":
